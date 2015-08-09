a846423d62b8679fd28fa66608fc46a4920b82b2
jython234 <jython234@hotmail.com>
2015-08-09 18:04:03 -0500

    Add stuff.

---
+++ src/main/java/net/beaconpe/raspberrytorch/RaspberryTorch.java
@@ -0,0 +1,12 @@
+package net.beaconpe.raspberrytorch;
+
+import net.beaconpe.jraklib.*;
+
+/**
+ * Created by jython234 on 8/9/2015.
+ *
+ * @author jython234
+ */
+public class RaspberryTorch {
+
+}
